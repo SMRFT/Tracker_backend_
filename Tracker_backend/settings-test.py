c2a604051574ad6c74a6232f90f743fcb4621c28
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-04-30 10:11:31 +0530

    Update settings-test.py

--- Tracker_backend/settings-test.py
+++ Tracker_backend/settings-test.py
@@ -116,7 +116,8 @@ DATABASES = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://testingtracker.netlify.app",  # your frontend domain
+    "https://testingtracker.netlify.app",
+    "https://testtracker.shinova.in/"  # your frontend domain
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
