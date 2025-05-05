c221162e5df016ca5735b1cb5416cb383714a7d9
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-05-05 14:47:14 +0530

    Update board.py

--- Tracker/Views/board.py
+++ Tracker/Views/board.py
@@ -33,7 +33,7 @@ db_name = os.environ.get("TRACKER_DB_NAME")
 if env_type == "test":
     client = MongoClient(mongo_uri)
 else:
-    client = MongoClient(mongo_uri, tls=True, tlsCAFile=certifi.where())
+    client = MongoClient(mongo_uri, tls=True,tlsAllowInvalidCertificates=True,tlsCAFile=certifi.where())
 
 
 # Initialize logging
