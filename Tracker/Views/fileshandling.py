a26d03b7b53814cd65316207af909d973e296903
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-07-07 16:08:51 +0530

    Update fileshandling.py

--- Tracker/Views/fileshandling.py
+++ Tracker/Views/fileshandling.py
@@ -19,7 +19,7 @@ load_dotenv()  # Load from .env if present
 
 env_type = os.environ.get("ENV_CLASSIFICATION", "local")
 
-mongo_uri = os.environ.get("TRACKER_DB_HOST")
+mongo_uri = os.environ.get("GLOBAL_DB_HOST")
 db_name = os.environ.get("TRACKER_DB_NAME")
        
 
