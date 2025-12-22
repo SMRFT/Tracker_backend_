81c559ad8b84c22487131f9c5f23f9b4096becdf
manibalan (SMRFT) <manibalansmrft@gmail.com>
2025-12-22 17:11:46 +0530

    Update settings-prod.py

--- Tracker_backend/settings-prod.py
+++ Tracker_backend/settings-prod.py
@@ -120,10 +120,3 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shanmugahospitalhr@gmail.com'
-EMAIL_HOST_PASSWORD = 'ykrs ylby ssro biwp'  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
