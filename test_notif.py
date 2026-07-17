226328d96a5a6602ce947295bc772bdc1280c99f
parthibanm-hash <parthiban.m@shinova.in>
2026-07-17 11:41:28 +0530

    update board and cards

--- test_notif.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracker_backend.settings')
-django.setup()
-from Tracker.models import Notification
-n = Notification.objects.last()
-print(f"n.id: {getattr(n, 'id', None)}, n.pk: {getattr(n, 'pk', None)}")
